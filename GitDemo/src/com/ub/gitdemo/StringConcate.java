6decc9b76744413ad9888201786d494575ca5008
ubhar <ubhar@DESKTOP-4MPV69Q>
2022-06-16 17:23:05 +0530

    create one string concate structure

---
+++ GitDemo/src/com/ub/gitdemo/StringConcate.java
@@ -0,0 +1,14 @@
+package com.ub.gitdemo;
+
+public class StringConcate {
+	public static String StringConcate () {
+		String s1="Punith";
+		String s2="Bhargav";
+		return s1 + s2;
+	}
+	public static void main(String[] args) {
+		System.out.println(StringConcate ());
+		
+	}
+	
+}
